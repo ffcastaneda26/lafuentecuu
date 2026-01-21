946be4720303cb7d1d79708619f450c86c1a338e
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2026-01-20 23:43:46 -0600

    Quitar vistas de noticia individual

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -84,15 +84,17 @@
                 @endif
 
                 <!-- Metadatos -->
-                <div class="flex items-center text-sm text-gray-500 mb-6 pb-6 border-b">
-                    <div class="flex items-center mr-6">
+                <div class="flex items-center justify-between w-full text-sm text-gray-500 mb-6 pb-6 border-b">
+
+                    <div class="flex items-center">
                         <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
                                 clip-rule="evenodd" />
                         </svg>
                         <span>{{ $news->user->name }}</span>
                     </div>
-                    <div class="flex items-center mr-6">
+
+                    <div class="flex items-center">
                         <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd"
                                 d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
@@ -100,15 +102,7 @@
                         </svg>
                         <span>{{ $news->published_at->format('d/m/Y H:i') }}</span>
                     </div>
-                    <div class="flex items-center">
-                        <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                            <path fill-rule="evenodd"
-                                d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                                clip-rule="evenodd" />
-                        </svg>
-                        <span>{{ $news->views_count }} vistas</span>
-                    </div>
+
                 </div>
 
                 <!-- Imagen Principal -->
@@ -339,7 +333,7 @@
                             <li class="border-b pb-3 last:border-b-0">
                                 <a href="{{ route('news.show', $popular->slug) }}" class="hover:text-blue-600">
                                     <div class="font-semibold text-sm line-clamp-2">{{ $popular->title }}</div>
-                                    <span class="text-xs text-gray-500">{{ $popular->views_count }} vistas</span>
+                                    {{-- <span class="text-xs text-gray-500">{{ $popular->views_count }} vistas</span> --}}
                                 </a>
                             </li>
                         @endforeach
